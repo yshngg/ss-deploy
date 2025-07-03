eb7f117d90847a43697b3c53686913296c2e995f
Yusheng Guo <113574546+yshngg@users.noreply.github.com>
2025-07-03 22:19:09 +0800

    Delete ip-check.sh

--- ip-check.sh
+++
@@ -1,6 +0,0 @@
-sudo docker run -d \
-  --name nginx-check \
-  --restart unless-stopped \
-  -p 80:80 \
-  --rm \
-  nginx:alpine sh -c 'echo "Hello, world!" > /usr/share/nginx/html/index.html && exec nginx -g "daemon off;"'
